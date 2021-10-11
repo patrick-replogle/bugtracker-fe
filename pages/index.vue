5cad68164ddfedcfc21213d42349ddae5aea0e62
patrick-replogle <patrickr1138@gmail.com>
2021-10-11 15:19:33 -0700

    fixed landing page styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <b-card class="card">
       <b-row no-gutters>
         <b-col md="6">
@@ -36,6 +36,40 @@
         </b-col>
       </b-row>
     </b-card>
+  </div> -->
+  <div class="wrapper">
+    <div class="card">
+      <div class="left"> 
+        <img
+          src="~/assets/img/software-development.jpg"
+          alt="Image"
+          class="rounded-0"
+        />
+      </div>
+      <div class="right">
+        <h1 class="title">Bugtracker</h1>
+        <b-card-text class="text">
+          Bugtracker is a free web app designed to help you keep of track
+          ongoing bugs, errors or issues in your software. Create new
+          projects and add users to your team. Generate and assign tickets
+          to teammates based on priority level. We help you keep track of
+          the details, so you can focus on the big picture.
+        </b-card-text>
+        <p>Create a new account or login below.</p>
+        <b-button
+          type="submit"
+          variant="outline-primary"
+          @click="handleClick('/register')"
+          >Register</b-button
+        >
+        <b-button
+          type="reset"
+          variant="primary"
+          @click="handleClick('/login')"
+          >Login</b-button
+        >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -52,7 +86,7 @@ export default {
   created() {
     axios
       .get(this.$config.baseURL + "/wakeup/heroku")
-      .then((res) => {
+      .then(() => {
         console.log("server awake");
       })
       .catch((err) => {
@@ -63,41 +97,78 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container {
+.wrapper {
   display: flex;
   justify-content: center;
 
   .card {
+    display: flex;
+    flex-direction: row;
     width: 95%;
+    max-width: 1100px;
     margin-top: 5vh;
-  }
-
-  .title {
-    font-size: 4rem;
-    @media (max-width: 600px) {
-      font-size: 2.5rem;
+    @media (max-width: 800px) {
+      flex-direction: column;
     }
-  }
 
-  .text {
-    font-size: 1.6rem;
-    margin: 3.5% 0;
-    @media (max-width: 600px) {
-      font-size: 1.4rem;
+    .left {
+      width: 50%;
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: baseline;
+      @media (max-width: 800px) {
+        width: 100%;
+      }
+      img {
+        object-fit: cover;
+        width: 100%;
+        max-height: 300px;
+        min-height: 300px;
+        @media (max-width: 920px) {
+          max-height: 330px;
+          min-height: 330px;
+        }
+      }
     }
-  }
 
-  p {
-    font-size: 1.2rem;
-  }
+    .right {
+      width: 50%;
+      padding: 10px;
+      line-height: 1.4;
+      margin-top: 10px;
+      @media (max-width: 800px) {
+        width: 100%;
+        margin-top: 5px;
+      }
+      .title {
+        font-size: 4rem;
+        @media (max-width: 800px) {
+          font-size: 2.5rem;
+        }
+      }
 
-  button {
-    width: 46%;
-    font-size: 1.6rem;
-    margin: 1%;
+      .text {
+        font-size: 1.6rem;
+        margin: 3.5% 0;
+        @media (max-width: 800px) {
+          font-size: 1.4rem;
+        }
+      }
 
-    @media (max-width: 600px) {
-      font-size: 1.4rem;
+      p {
+        font-size: 1.4rem;
+      }
+
+      button {
+        width: 46%;
+        font-size: 1.6rem;
+        margin-right: 1%;
+
+        @media (max-width: 800px) {
+          font-size: 1.4rem;
+        }
+      }
     }
   }
 }
